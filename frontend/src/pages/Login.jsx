58a2981101d79981de03bb9ebd8065023b1e413c
PoojaChippa <poojachippa04@gmail.com>
2026-03-15 16:24:55 +0530

    added the context and pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const res = await API.post("/auth/login", {
+      email,
+      password,
+    });
+
+    login(res.data.token);
+
+    navigate("/"); // redirect to dashboard
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+
+export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const signup = async () => {
+    await API.post("/auth/signup", {
+      name,
+      email,
+      password,
+    });
+
+    alert("Signup Successful");
+
+    navigate("/login"); // redirect to login
+  };
+
+  return (
+    <div className="container">
+      <h2>Signup</h2>
+
+      <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
+}
